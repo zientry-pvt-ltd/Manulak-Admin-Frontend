80f4351fc5d7b3a544e010d7c749c87bdb0c6436
dulaj96 <dulajmithun96@gmail.com>
2025-06-14 22:44:28 +0530

    feat: Update copyright year in Footer component

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-gray-800 text-white p-4 text-center text-sm">
+    <footer className="bg-white text-black font-bold p-4 text-center text-sm">
       Copyright © {currentYear} - All right reserved by NexPhere Solutions
     </footer>
   );
