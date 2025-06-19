407ebb9f562241019b5c96c0015c6c537ef0fbe9
Charith Jayodya <charithjdesilva@gmail.com>
2025-06-19 19:52:45 +0530

    Created footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './components/ui/custom/Footer'
 import StockFeature from './features/stock-feature/StockFeature'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <StockFeature />
+      <Footer />
     </>
   )
 }
---
+++ src/components/ui/custom/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="p-4 mt-8 text-sm w-full bg-background fixed bottom-0 inset-x-0">
+            <p>Copyright © 2025 - All right reserved by NexPhere Solutions(Pvt.) Ltd</p>
+        </div>
+    );
+}
+
+export default Footer;
