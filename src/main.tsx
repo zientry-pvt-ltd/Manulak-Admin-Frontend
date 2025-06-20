ad57a4f3ade25a239f92878ea57d7dad9616eda4
arundeshan@gmail.com <arundeshan@gmail.com>
2025-06-20 16:03:39 +0530

    fix: update import path for store in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,9 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
+import { persistor, store } from "@/store";
+
 import App from "./App";
-import { persistor, store } from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
