5bb21220b9afb4871b15da9e25ec5ed8f4dbd544
arundeshan@gmail.com <arundeshan@gmail.com>
2025-06-24 01:47:45 +0530

    fix: update persistConfig to whitelist the app slice for persistence

--- src/store/index.ts
+++ src/store/index.ts
@@ -22,7 +22,7 @@ const rootReducer = combineReducers({
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: [], // Only persist `app` slice
+  whitelist: ["app"], // Only persist `app` slice
   blacklist: [], // Add slices to exclude here
 };
 
