d95c3ff58d45211b6163597f58c732acafdd5060
arundeshan@gmail.com <arundeshan@gmail.com>
2025-06-27 00:16:34 +0530

    feat: enhance AppIcon component to accept custom className for styling flexibility

--- src/components/ui/app-icon.tsx
+++ src/components/ui/app-icon.tsx
@@ -1,21 +1,26 @@
 import { type LucideProps } from "lucide-react";
 import type { ForwardRefExoticComponent, RefAttributes } from "react";
 
+import { cn } from "@/lib/utils";
+
 interface AppIconProps {
   Icon: ForwardRefExoticComponent<
     Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>
   >;
   size?: "sm" | "md" | "lg";
+  className?: string;
 }
 
-const AppIcon = ({ Icon, size = "md" }: AppIconProps) => {
+const AppIcon = ({ Icon, size = "md", className }: AppIconProps) => {
   const iconSizeMap: Record<NonNullable<AppIconProps["size"]>, string> = {
     sm: "size-4",
     md: "size-5",
     lg: "size-6",
   };
 
-  return <Icon strokeWidth={1.3} className={iconSizeMap[size]} />;
+  return (
+    <Icon strokeWidth={1.3} className={cn(iconSizeMap[size], className)} />
+  );
 };
 
 export default AppIcon;
