b45c059ce9bf9802cc73cd27fda587d014fd1893
arundeshan@gmail.com <arundeshan@gmail.com>
2025-06-26 23:20:30 +0530

    feat: refactor UsersRoute to remove unused code and showcase components

--- src/app/routes/app/dashboard.tsx
+++ src/app/routes/app/dashboard.tsx
@@ -1,56 +1,21 @@
-import { PlusCircle } from "lucide-react";
-
-import { AppButton, AppText } from "@/components";
 import { ContentLayout } from "@/components/layouts";
+import AppIconButtonShowcase from "@/components/temp/AppIconButtonShowcase";
+import AppTextShowcase from "@/components/temp/AppTextShowcase";
+import ButtonShowcase from "@/components/temp/ButtonShowcase";
 import { ROLES } from "@/constants";
 import { Authorization } from "@/lib/authorization";
-import { fetchUserProfile, logout, setError } from "@/store/slices/authSlice";
-import { useAppDispatch, useAppDispatchAll } from "@/store/utils";
 
 const UsersRoute = () => {
-  const dispatch = useAppDispatch();
-  const dispatchAll = useAppDispatchAll();
-
-  const fetchData = async () => {
-    try {
-      await dispatch(fetchUserProfile());
-    } catch (err) {
-      console.error("Error fetching user:", err);
-    }
-  };
-
   return (
     <ContentLayout title="Dashboard">
       <Authorization
         forbiddenFallback={<div>Only admin can view this.</div>}
         allowedRoles={[ROLES.ADMIN]}
       >
-        <AppText variant="caption">Dashboard Content</AppText>
-
         <div className="flex gap-4 mt-3">
-          <AppButton
-            rounded="full"
-            size="sm"
-            onClick={fetchData}
-            variant="default"
-          >
-            Fetch User Profile
-          </AppButton>
-
-          <AppButton
-            variant={"outline"}
-            size="sm"
-            rounded="full"
-            onClick={() => {
-              dispatchAll([logout(), setError("error message")]);
-            }}
-          >
-            Log out
-          </AppButton>
-
-          <AppButton Icon={PlusCircle} size="sm" variant="outline">
-            Add Item
-          </AppButton>
+          <ButtonShowcase />
+          <AppTextShowcase />
+          <AppIconButtonShowcase />
         </div>
       </Authorization>
     </ContentLayout>
