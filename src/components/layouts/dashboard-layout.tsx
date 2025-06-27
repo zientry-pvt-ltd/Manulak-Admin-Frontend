d8a7ac6098f20edee2dd897f2ad7fc5594ddd4e7
arundeshan@gmail.com <arundeshan@gmail.com>
2025-06-27 12:54:18 +0530

    feat: simplify DashboardLayout by removing unnecessary components and using AppContentHeader

--- src/components/layouts/dashboard-layout.tsx
+++ src/components/layouts/dashboard-layout.tsx
@@ -1,10 +1,6 @@
-import { ChevronRight } from "lucide-react";
-
-import AppButton from "@/components/ui/app-button";
+import AppContentHeader from "@/components/ui/app-content-header";
 import AppFooter from "@/components/ui/app-footer";
-import AppIcon from "@/components/ui/app-icon";
 import { AppSidebar } from "@/components/ui/app-sidebar";
-import AppText from "@/components/ui/app-text";
 import { SidebarProvider } from "@/components/ui/sidebar";
 
 export function DashboardLayout({ children }: { children: React.ReactNode }) {
@@ -13,14 +9,7 @@ export function DashboardLayout({ children }: { children: React.ReactNode }) {
       <AppSidebar />
       <main className="w-full flex flex-col justify-between">
         {/* side bar trigger header */}
-        <div className="flex items-center bg-sidebar h-[6vh] px-2">
-          <AppText variant="caption">Dashboard</AppText>
-          <AppIcon Icon={ChevronRight} size="sm" />
-          <AppButton size="sm" className="ml-auto">
-            Add Item
-          </AppButton>
-        </div>
-
+        <AppContentHeader />
         {children}
         <AppFooter />
       </main>
