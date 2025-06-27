28fed8191d6ceba50a737b708fd3b2ef8d88ea37
arundeshan@gmail.com <arundeshan@gmail.com>
2025-06-27 12:54:07 +0530

    feat: remove appName display from AppTitle component for cleaner UI

--- src/components/ui/app-title.tsx
+++ src/components/ui/app-title.tsx
@@ -1,12 +1,12 @@
 import { memo } from "react";
 
-import { AppImage, AppText, useSidebar } from "@/components";
+import { AppImage, useSidebar } from "@/components";
 import { cn } from "@/lib/utils";
 import { selectApp } from "@/store/selectors/appSelectors";
 import { useAppSelector } from "@/store/utils";
 
 const AppTitle = () => {
-  const { appName, appLogo } = useAppSelector(selectApp);
+  const { appLogo } = useAppSelector(selectApp);
   const { state } = useSidebar();
 
   const isCollapsed = state === "collapsed";
@@ -18,16 +18,13 @@ const AppTitle = () => {
         isCollapsed && "justify-center px-0",
       )}
     >
-      <AppImage imageUrl={appLogo} alt="App Logo" />
-
-      <AppText
-        as="span"
-        variant="label"
-        ellipsis
-        className={cn(isCollapsed ? "hidden" : "block")}
-      >
-        {appName}
-      </AppText>
+      <AppImage
+        width={isCollapsed ? 24 : 120}
+        height={isCollapsed ? 24 : 40}
+        imageUrl={isCollapsed ? appLogo[0] : appLogo[1]}
+        alt="App Logo"
+        rounded="rounded-none"
+      />
     </div>
   );
 };
