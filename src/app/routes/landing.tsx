d1edf5ac8493beacdd1501b2e6c6acd1d555670a
arundeshan@gmail.com <arundeshan@gmail.com>
2025-07-01 00:27:45 +0530

    fix: implement authentication check in LandingRoute navigation

--- src/app/routes/landing.tsx
+++ src/app/routes/landing.tsx
@@ -2,15 +2,25 @@ import { useNavigate } from "react-router";
 
 import { Button } from "@/components";
 import { paths } from "@/config/paths";
+import { useAuth } from "@/features/auth";
 
 const LandingRoute = () => {
   const navigate = useNavigate();
+  const { isAuthenticated } = useAuth();
 
   const handleStart = () => {
-    navigate(paths.app.dashboard.getHref());
+    if (!isAuthenticated) {
+      navigate(paths.auth.login.getHref(location.pathname), { replace: true });
+    } else {
+      navigate(paths.app.dashboard.getHref(), { replace: true });
+    }
   };
 
-  return <Button onClick={handleStart}>Started</Button>;
+  return (
+    <Button onClick={handleStart} className="m-auto mt-20 flex">
+      Started
+    </Button>
+  );
 };
 
 export default LandingRoute;
