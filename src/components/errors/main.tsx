501dcaeb7bb4b30d569296d5d94254597721a1ef
arundeshan@gmail.com <arundeshan@gmail.com>
2025-07-02 01:55:56 +0530

    feat: simplify MainErrorFallback component

--- src/components/errors/main.tsx
+++ src/components/errors/main.tsx
@@ -3,14 +3,13 @@ import { AppButton, AppText } from "@/components";
 export const MainErrorFallback = () => {
   return (
     <div
-      className="flex h-screen w-screen flex-col items-center justify-center text-red-500"
+      className="flex h-screen w-screen flex-col items-center justify-center"
       role="alert"
     >
-      <AppText variant="label">Ooops, something went wrong :( </AppText>
-      <AppButton
-        size="md"
-        onClick={() => window.location.assign(window.location.origin)}
-      >
+      <AppText variant="label" color="destructive">
+        Ooops, something went wrong :(
+      </AppText>
+      <AppButton onClick={() => window.location.assign(window.location.origin)}>
         Refresh
       </AppButton>
     </div>
