d6c5a4bff62ec4e8226e1802f58f9186e752a64b
arundeshan@gmail.com <arundeshan@gmail.com>
2025-09-12 00:45:13 +0530

    fix: update AppMetadata to use faviconUrl instead of appLogo for icon rendering

--- src/components/seo/app-metadata.tsx
+++ src/components/seo/app-metadata.tsx
@@ -1,13 +1,13 @@
 import { useApp } from "@/features/settings";
 
 export const AppMetadata = () => {
-  const { appName, appLogo } = useApp();
+  const { appName, faviconUrl } = useApp();
 
   return (
     <>
       <title>{appName || "Loading..."}</title>
       <meta name="application-name" content={appName} />
-      <link rel="icon" href={appLogo[0]} />
+      <link rel="icon" href={faviconUrl} />
     </>
   );
 };
