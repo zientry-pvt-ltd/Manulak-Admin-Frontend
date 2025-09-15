07c10e0fbdcf4ab7fe104d514593cce1c1afa308
arundeshan@gmail.com <arundeshan@gmail.com>
2025-09-16 00:22:08 +0530

    fix: update IProductInfo fields to use number type for price and weight attributes

--- src/features/products/types/product.type.ts
+++ src/features/products/types/product.type.ts
@@ -1,3 +1,4 @@
+import type { FormFieldValues } from "@/features/products/components/product-form";
 import type { ApiResource, ApiResourceList } from "@/types";
 
 export type ProductCategoryType =
@@ -12,11 +13,11 @@ export type IProductInfo = {
   product_name: string;
   product_desc: string;
   product_category: string;
-  bought_price: string;
-  selling_price: string;
-  unit_weight: string;
-  courier_chargers_1kg: string;
-  courier_chargers_more_than_1kg: string;
+  bought_price: number;
+  selling_price: number;
+  unit_weight: number;
+  courier_chargers_1kg: number;
+  courier_chargers_more_than_1kg: number;
   product_image_urls: string[];
   created_at: string;
   updated_at: string;
@@ -36,6 +37,8 @@ export interface IUpdateProductRequest {
   product_image_urls?: string[];
   is_deleted?: boolean;
 }
+
+export type IProductCreateRequest = FormFieldValues;
 
 export type IProductListResponse = ApiResourceList<IProductInfo>;
 
