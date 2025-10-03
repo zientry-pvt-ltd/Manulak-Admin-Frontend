52c59f13411c349d3b62b2b814e717b5eed5fe3f
arundeshan@gmail.com <arundeshan@gmail.com>
2025-10-04 00:02:04 +0530

    feat: update stock types to include StockOperationType and enhance IStockInfo structure

--- src/features/stock/types/stock.type.ts
+++ src/features/stock/types/stock.type.ts
@@ -1,16 +1,24 @@
+import type { IProductInfo } from "@/features/products/types/product.type";
+import type { StockOperations } from "@/features/stock/constants";
 import type { ApiResource, ApiResourceList } from "@/types";
 
-export type IStockInfo = {
-  id: string;
-  product_name: string;
-  category: string;
-  quantity: number;
-  unit_price: number;
-  net_worth: number;
-};
+export type StockOperationType = keyof typeof StockOperations;
 
-export type IUpdateStockQuantityRequest = Partial<IStockInfo> & { id: string };
+export type IStockInfo = Pick<
+  IProductInfo,
+  "product_name" | "product_category" | "quantity" | "bought_price"
+>;
+
+export type IUpdateStockQuantityRequest = {
+  operation: StockOperationType;
+  quantity: number;
+};
 
 export type IStockListResponse = ApiResourceList<IStockInfo>;
 
 export type IStockResponse = ApiResource<IStockInfo>;
+
+export type ICreateStockRequest = {
+  product_id: string;
+  quantity: number;
+};
